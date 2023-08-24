9f29fccb9bf1700996e482c48f7a1eb7364faef2
Ceci Castillo <>
2023-08-24 04:59:02 -0600

    Compose Lab Solution Card

---
+++ LabCompose2/app/src/main/java/com/zezzi/labcompose2/CardItem.kt
@@ -0,0 +1,3 @@
+package com.zezzi.labcompose2
+
+class CardItem(var recipe: String, var image: String)
---
+++ LabCompose2/app/src/main/java/com/zezzi/labcompose2/DetailScreen.kt
@@ -0,0 +1,6 @@
+package com.zezzi.labcompose2
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DetailScreen() {}
