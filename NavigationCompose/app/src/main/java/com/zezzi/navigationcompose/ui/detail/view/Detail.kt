1aaf168218b0e66a032e69ba7105bf68d686258e
Ceci Castillo <>
2023-08-27 22:04:26 -0600

    Navigation Example - Compose Navigation Example Code

--- NavigationCompose/app/src/main/java/com/zezzi/navigationcompose/ui/detail/view/Detail.kt
+++ NavigationCompose/app/src/main/java/com/zezzi/navigationcompose/ui/detail/view/Detail.kt
@@ -2,6 +2,7 @@ package com.zezzi.navigationcompose.ui.detail.view
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -9,8 +10,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.zezzi.navigationcompose.R
 import com.zezzi.navigationcompose.navigation.AppBar
@@ -23,15 +26,19 @@ fun Detail(navController: NavController) {
     Scaffold(topBar = {
         AppBar(title = "Detail", navController = navController)
     }) {
-        Column {
-            Button(onClick = { navController.navigate(route = NavigationState.Profile.route) }) {
-                Text(text = "Test Navigation")
+        Column(modifier = Modifier.fillMaxWidth()
+            .padding(16.dp),
+            horizontalAlignment = Alignment.Start) {
+            Column {
+                Text(
+                    text = "Detail Event",
+                    style = MaterialTheme.typography.labelMedium,
+                    modifier = Modifier.padding(start = dimensionResource(R.dimen.padding_small))
+                )
+                Button(onClick = { navController.navigate(route = NavigationState.Profile.route) }) {
+                    Text(text = "Test Navigation")
+                }
             }
-            Text(
-                text = "Detail Event",
-                style = MaterialTheme.typography.labelMedium,
-                modifier = Modifier.padding(start = dimensionResource(R.dimen.padding_small))
-            )
         }
     }
 
