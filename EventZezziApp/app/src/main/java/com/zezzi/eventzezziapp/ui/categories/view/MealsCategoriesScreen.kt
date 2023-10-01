a7a918beda3f01e779da33691e33dd90f5019062
Ceci Castillo <>
2023-10-01 14:53:06 -0600

    Creating Coroutines Third Option

--- EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/ui/categories/view/MealsCategoriesScreen.kt
+++ EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/ui/categories/view/MealsCategoriesScreen.kt
@@ -1,25 +1,102 @@
 package com.zezzi.eventzezziapp.ui.categories.view
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import coil.compose.rememberImagePainter
+import com.zezzi.eventzezziapp.R
+import com.zezzi.eventzezziapp.navigation.AppBar
+import com.zezzi.eventzezziapp.networking.response.Categories
 import com.zezzi.eventzezziapp.ui.categories.viewmodel.MealsCategoriesViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import com.zezzi.eventzezziapp.ui.concerts.view.ConcertsGrid
+import com.zezzi.eventzezziapp.ui.theme.EventZezziAppTheme
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MealsCategoriesScreen(navController: NavController) {
     val viewModel: MealsCategoriesViewModel = viewModel()
     val meals = viewModel.mealsState.value
 
-    LazyColumn {
-        items(meals) { meal ->
-            Text(text = meal.name)
+    Scaffold(topBar = {
+        AppBar(title = "Categories", navController = navController)
+    }) {
+        LazyColumn(contentPadding = PaddingValues(16.dp)) {
+            items(meals) { meal ->
+                MealCategory(meal)
+            }
         }
+    }
+}
+
+@Composable
+fun MealCategory(meal: Categories) {
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        elevation = 2.dp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp)
+    ) {
+        Row {
+            Image(
+                painter = rememberImagePainter(meal.imageUrl),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(88.dp)
+                    .padding(4.dp)
+            )
+            Column(
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.category_name),
+                    style = MaterialTheme.typography.h6
+                )
+                Divider()
+                Text(
+                    text = meal.name,
+                    style = MaterialTheme.typography.h6
+                )
+                Divider()
+                Text(
+                    text = stringResource(R.string.date_content, 11, 12),
+                    style = MaterialTheme.typography.h6
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    EventZezziAppTheme {
+        MealsCategoriesScreen(navController = rememberNavController())
     }
 }
--- EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/ui/categories/viewmodel/MealsCategoriesViewModel.kt
+++ EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/ui/categories/viewmodel/MealsCategoriesViewModel.kt
@@ -1,10 +1,12 @@
 package com.zezzi.eventzezziapp.ui.categories.viewmodel
 
+import android.util.Log
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.lifecycle.ViewModel
 import androidx.compose.runtime.remember
+import androidx.lifecycle.viewModelScope
 import com.zezzi.eventzezziapp.networking.response.Categories
 import com.zezzi.eventzezziapp.ui.categories.repository.MealsRepository
 import kotlinx.coroutines.CoroutineScope
@@ -16,18 +18,15 @@ class MealsCategoriesViewModel (private val repository: MealsRepository = MealsR
 
     val mealsState: MutableState<List<Categories>> =  mutableStateOf(emptyList<Categories>())
 
-    private val mealsJob = Job()
     init {
-        val scope = CoroutineScope(mealsJob + Dispatchers.IO)
-        scope.launch(Dispatchers.IO) {
+        Log.d("TAG_COROUTINES", "about to launch a coroutine")
+        viewModelScope.launch(Dispatchers.IO) {
+            Log.d("TAG_COROUTINES", "launching a coroutine")
             val meals = getMealsCategories()
+            Log.d("TAG_COROUTINES", "we have received sync data")
             mealsState.value = meals
         }
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-        mealsJob.cancel()
+        Log.d("TAG_COROUTINES", "other work")
     }
 
     private suspend fun getMealsCategories(): List<Categories> {
